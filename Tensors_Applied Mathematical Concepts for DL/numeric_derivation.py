f52aa1bd1956fa21e354b273de8678f1e8b430e5
Mansi Rajadhyaksha <mansi0298@gmail.com>
2024-01-25 21:24:44 -0500

    Delete Tensors_Applied Mathematical Concepts for DL directory

--- Tensors_Applied Mathematical Concepts for DL/numeric_derivation.py
+++
@@ -1,4 +0,0 @@
-def derive(f, x, h=0.0001):
-    # Calculating the derivative using the definition: (f(x + h) - f(x - h)) / (2 * h)
-    derivative = (f(x + h) - f(x - h)) / (2 * h)
-    return derivative
